2af77e7692c253fd76c8c32d49b199b62a693fcb
louieddxu2 <louieddxu2@gmail.com>
2025-12-06 17:38:38 +0800

    feat: Update Great Western Trail template name and version cache
    Revises the template name for "Great Western Trail" to "Great Western Trail: USA" and clears the service worker cache. This ensures accurate template representation and forces a fresh download of assets.

--- src/constants.ts
+++ src/constants.ts
@@ -27,8 +27,8 @@ export const DEFAULT_TEMPLATES: GameTemplate[] = [
   },
   {
     "id": "sys_gwt_1",
-    "name": "大西部開拓者",
-    "description": "包含《北方鐵路》擴充計分項目。",
+    "name": "大西部開拓者：美利堅",
+    "description": "",
     "createdAt": 1700000000002,
     "columns": [
       {
@@ -162,22 +162,197 @@ export const DEFAULT_TEMPLATES: GameTemplate[] = [
       { "id": "c8", "name": "糧食/其他", "type": "number", "isScoring": true, "weight": 1, "unit": "分" }
     ]
   },
-  {
-    "id": "sys_gwt_nz",
-    "name": "大西部：紐西蘭 (GWT: New Zealand)",
-    "description": "包含剪毛、鳥類卡與商店獎勵。",
-    "createdAt": 1700000000004,
-    "columns": [
-      { "id": "c1", "name": "金錢", "type": "number", "isScoring": true, "weight": 0.2, "unit": "元 ($5=1分)", "rounding": "floor" },
-      { "id": "c2", "name": "建築物", "type": "number", "isScoring": true, "weight": 1, "unit": "分" },
-      { "id": "c3", "name": "城市運送", "type": "number", "isScoring": true, "weight": 1, "unit": "分" },
-      { "id": "c4", "name": "鳥類卡/羊毛", "type": "number", "isScoring": true, "weight": 1, "unit": "分" },
-      { "id": "c5", "name": "危險/導航軌", "type": "number", "isScoring": true, "weight": 1, "unit": "分" },
-      { "id": "c6", "name": "任務卡", "type": "number", "isScoring": true, "weight": 1, "unit": "分" },
-      { "id": "c7", "name": "玩家圖板解鎖", "type": "number", "isScoring": true, "weight": 1, "unit": "分" },
-      { "id": "c8", "name": "商店獎勵", "type": "number", "isScoring": true, "weight": 1, "unit": "分" }
-    ]
-  },
+{
+  "id": "7d844523-2a19-4b09-bc81-bd46d83d1e8d",
+  "name": "大西部開拓者：紐西蘭",
+  "description": "包含剪毛、鳥類卡與商店獎勵。",
+  "createdAt": 1765012461244,
+  "columns": [
+    {
+      "id": "c1",
+      "name": "金錢",
+      "type": "number",
+      "isScoring": true,
+      "weight": 0.2,
+      "unit": "元",
+      "rounding": "floor",
+      "mappingRules": []
+    },
+    {
+      "id": "c2",
+      "name": "建築物",
+      "type": "number",
+      "isScoring": true,
+      "weight": 1,
+      "unit": "分"
+    },
+    {
+      "id": "c3",
+      "name": "貿易站",
+      "type": "number",
+      "isScoring": true,
+      "weight": 1,
+      "unit": "分",
+      "mappingRules": []
+    },
+    {
+      "id": "5ac5b020-f457-451b-9ea5-b2946a8fd618",
+      "name": "港口",
+      "type": "number",
+      "isScoring": true,
+      "weight": 1,
+      "options": [],
+      "mappingRules": [],
+      "unit": "分",
+      "rounding": "none",
+      "quickButtons": []
+    },
+    {
+      "id": "0e5c54ab-8cc2-4441-93d2-e158e1d1673f",
+      "name": "開拓者軌",
+      "type": "number",
+      "isScoring": true,
+      "weight": 1,
+      "options": [],
+      "mappingRules": [
+        {
+          "score": 0,
+          "min": 0,
+          "max": 4
+        },
+        {
+          "score": 3,
+          "min": 5,
+          "max": 8
+        },
+        {
+          "score": 7,
+          "min": 9,
+          "max": 11
+        },
+        {
+          "score": 10,
+          "min": 12,
+          "max": 13
+        },
+        {
+          "score": 13,
+          "min": 14,
+          "max": 15
+        },
+        {
+          "score": 15,
+          "min": 16
+        }
+      ],
+      "unit": "格",
+      "rounding": "none",
+      "quickButtons": [],
+      "calculationType": "standard"
+    },
+    {
+      "id": "7cad37ab-86fe-4771-8d3b-fe9e12ae6346",
+      "name": "天災/獎勵板塊",
+      "type": "number",
+      "isScoring": true,
+      "weight": 1,
+      "options": [],
+      "mappingRules": [],
+      "unit": "分",
+      "rounding": "none",
+      "quickButtons": []
+    },
+    {
+      "id": "c4",
+      "name": "綿羊卡與獎勵卡",
+      "type": "number",
+      "isScoring": true,
+      "weight": 1,
+      "unit": "分",
+      "mappingRules": []
+    },
+    {
+      "id": "c5",
+      "name": "目標卡",
+      "type": "number",
+      "isScoring": true,
+      "weight": 1,
+      "unit": "分",
+      "mappingRules": []
+    },
+    {
+      "id": "c6",
+      "name": "港務長板塊",
+      "type": "number",
+      "isScoring": true,
+      "weight": 1,
+      "unit": "分",
+      "mappingRules": []
+    },
+    {
+      "id": "c7",
+      "name": "移動3分解鎖",
+      "type": "select",
+      "isScoring": true,
+      "weight": 1,
+      "unit": "",
+      "options": [
+        {
+          "value": 3,
+          "label": "有"
+        },
+        {
+          "value": 0,
+          "label": "無"
+        }
+      ],
+      "calculationType": "standard"
+    },
+    {
+      "id": "c8",
+      "name": "工人第5格",
+      "type": "number",
+      "isScoring": true,
+      "weight": 4,
+      "unit": "位",
+      "mappingRules": []
+    },
+    {
+      "id": "37cef49b-1f3c-4bb4-94c4-7555fef58b12",
+      "name": "倉庫",
+      "type": "number",
+      "isScoring": true,
+      "weight": 1,
+      "options": [],
+      "mappingRules": [],
+      "unit": "分",
+      "rounding": "none",
+      "quickButtons": []
+    },
+    {
+      "id": "cf6d8175-2279-4a5d-9d92-8a83f9a2196b",
+      "name": "結束遊戲",
+      "type": "select",
+      "isScoring": true,
+      "weight": 1,
+      "options": [
+        {
+          "value": 5,
+          "label": "是"
+        },
+        {
+          "value": 0,
+          "label": "否"
+        }
+      ],
+      "mappingRules": [],
+      "unit": "",
+      "rounding": "none",
+      "quickButtons": [],
+      "calculationType": "standard"
+    }
+  ]
+},
   {
     "id": "sys_iaww",
     "name": "美麗新世界 (It's a Wonderful World)",
